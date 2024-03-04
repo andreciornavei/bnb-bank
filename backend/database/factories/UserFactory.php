5a9aef85e55ea6c367bfb8d5b5b205ce98e16d3d
André Ciornavei <andre.ciornavei@gmail.com>
2024-03-04 13:32:23 -0300

    adjust users collection and seeds

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,21 +24,21 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'balance' => 0,
+            'role' => 'customer' 
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the model's role should be admin.
      */
-    public function unverified(): static
+    public function asAdmin(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'role' => 'admin',
         ]);
     }
 }
