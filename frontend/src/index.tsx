89376dd1fe6eb5e6daf9f6a12284fd6ba9dd432e
André Ciornavei <andre.ciornavei@gmail.com>
2024-03-08 13:18:27 -0300

    added sentry as monitoring tool on frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import './index.css'
 import Router from './router'
 import { enableMapSet } from 'immer'
 import { DefaultTheme } from './theme'
+import * as Sentry from '@sentry/react'
 import ReactDOM from 'react-dom/client'
 import { ApiProvider } from '@hooks/api'
 import { AuthProvider } from '@hooks/auth'
@@ -10,6 +11,25 @@ import { ThemeProvider } from '@mui/material'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 enableMapSet()
+
+// Install Sentry For Monitoring
+Sentry.init({
+  dsn: 'https://56ff0892b732563d22086de012c5a22c@o4506876266741760.ingest.us.sentry.io/4506876269494272',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: [
+    'localhost',
+    /^https:\/\/bnb-bank.andreciornavei\.com\.br/,
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
